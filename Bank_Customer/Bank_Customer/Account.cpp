16f9d205e8bf877c55555e5f64f0af3165e5969a
trevcarlson <trevor.carlson.mn@gmail.com>
2014-12-14 17:03:22 -0600

    Cleaned up naming for account and transaction cpp files

--- Bank_Customer/Bank_Customer/Account.cpp
+++ Bank_Customer/Bank_Customer/Account.cpp
@@ -28,22 +28,22 @@ vector<Transaction*>& Account::getTransactions() { return pTransactions; } //Cha
 //Setters
 void Account::setType(int Type) { type = Type; }
 
-void Account::setNumber(int NUMBER) { account_number = NUMBER; }
+void Account::setNumber(int Number) { account_number = Number; }
 
-void Account::setBalance(double BALANCE) { opening_balance = BALANCE; }
+void Account::setBalance(double Balance) { opening_balance = Balance; }
 
-void Account::setOwner(Customer* OWNER) { pOwners.push_back(OWNER); }
+void Account::setOwner(Customer* Owner) { pOwners.push_back(Owner); }
 
-void Account::setDate(Date DATE) { opening_date = DATE; }
+void Account::setDate(Date Date) { opening_date = Date; }
 
-void Account::setTransaction(Transaction* TRANSACTION) { pTransactions.push_back(TRANSACTION); }
+void Account::setTransaction(Transaction* Transaction) { pTransactions.push_back(Transaction); }
 
 //Operators
-istream& operator>>(istream& INPUT, vector<Account*>& vector) {
+istream& operator>>(istream& Input, vector<Account*>& vector) {
 	int type, number;
 	double balance;
 	Date date;
-	INPUT >> type >> number >> balance >> date;
+	Input >> type >> number >> balance >> date;
 	if (type == 1) {
 		Checking_Account* new_account = new Checking_Account(number, balance, date);
 		vector.push_back(new_account);
@@ -55,17 +55,17 @@ istream& operator>>(istream& INPUT, vector<Account*>& vector) {
 	else if (type == 3) {
 		double interest_rate;
 		Date maturity_date;
-		INPUT >> interest_rate >> maturity_date;
+		Input >> interest_rate >> maturity_date;
 		CD_Account* new_account = new CD_Account(number, balance, date, interest_rate, maturity_date);
 		vector.push_back(new_account);
 	}
 	else if (type == 4) {
 		double interest_rate;
-		INPUT >> interest_rate;
+		Input >> interest_rate;
 		Loan* new_account = new Loan(number, balance, date, interest_rate);
 		vector.push_back(new_account);
 	}
-	return INPUT;
+	return Input;
 }
 
 //Functions
