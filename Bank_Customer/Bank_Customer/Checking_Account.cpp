a4f0ebc90ab2991fe48d40b6effd8240f3d00799
Samantha Rouillard <samantha.rouillard23@gmail.com>
2014-12-14 17:10:26 -0600

    Checking Account cleanup

--- Bank_Customer/Bank_Customer/Checking_Account.cpp
+++ Bank_Customer/Bank_Customer/Checking_Account.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Constructor
 Checking_Account::Checking_Account(int number, double balance, Date& date) : Account(number, balance, date)
 {
 	type = 1;
@@ -11,5 +12,11 @@ Checking_Account::Checking_Account(int number, double balance, Date& date) : Acc
 	overdraft_fee = 10;
 }
 
+//Getters
 double Checking_Account::getMonthlyFee() { return monthly_fee; }
 double Checking_Account::getOverdraftFee() { return overdraft_fee; }
+
+//Setters
+void Checking_Account::setMonthlyFee(double monthly_fee) { monthly_fee = 5; }
+void Checking_Account::setOverdraftFee(double overdraft_fee) { overdraft_fee = 10; }
+
