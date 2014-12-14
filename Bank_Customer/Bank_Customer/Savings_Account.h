2fbd92647a2a0b06f1277143f166314fe62fc9d1
Samantha Rouillard <samantha.rouillard23@gmail.com>
2014-12-14 17:02:21 -0600

    Cleaning code/Adding comments

--- Bank_Customer/Bank_Customer/Savings_Account.h
+++ Bank_Customer/Bank_Customer/Savings_Account.h
@@ -4,10 +4,15 @@
 
 
 class Savings_Account : public Account {
+
 public:
+	//Constructors
 	Savings_Account();
 	Savings_Account(int, double, Date&);
+
+	//Getters
 	double getInterestRate();
+
 private:
 	double interest_rate;
 };
